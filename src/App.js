2bbabf8b77fc87ef0933c6d834cb4cfd71b2014b
Muhammad Saqib Khan <niazisaqib991@gmail.com>
2023-02-19 10:00:11 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  
 
+  const [news, setNews] = useState([])
+  useEffect(() => {
+    axios
+    .get(" https://newsapi.org/v2/top-headlines?country=in&apiKey=7698789c567e424c83879002986c862e")
+      .then((res) => {
+        setNews(res.data.articles);
+      })
+
+  }, [])
+  return (
+    <>
+      <div className='container my-5'>
+        <div className='row text-center'>
+          {
+            news.map((val) => {
+              return (
+                <div className='col my-3'>
+                  <div class="card" style={{ width: "18rem" }}>
+                    <img src={val.urlToImage} class="card-img-top" alt="..." />
+                    <div class="card-body">
+                      <h5 class="card-title">{val.title}</h5>
+                      <p class="card-text">{val.description}</p>
+                      
+                    </div>
+                  </div>
+
+                </div>
+
+              )
+            })
+          }
+        </div>
+      </div>
+
+    </>
+  )
+}
 export default App;
